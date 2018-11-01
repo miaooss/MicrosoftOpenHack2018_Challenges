bbded55ebad25d0985f9379bfd28df2b3a4d56ec
Patricio <pbustos@safefleet.net>
2018-11-01 13:43:01 -0400

    Update Version

--- OpenHack.Challenge02/OpenHack.Challenge02.API/CreateRating.cs
+++ OpenHack.Challenge02/OpenHack.Challenge02.API/CreateRating.cs
@@ -56,7 +56,7 @@ namespace OpenHack.Challenge02.API
             rating.Rating = data?.rating;
 			rating.LocationName = data?.locationName;
             rating.Rating = data?.rating;
-            rating.Version = "1.1";
+            rating.Version = "1.2";
 
             var result = await Infrastructure.UserRatingManager.AddAsync(rating);
 
